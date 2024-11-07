3e4d782d045a9d1d7133435604a189b37dbf5467
Eduardo Aguilar <es123y123@gmail.com>
2024-11-07 12:15:17 -0500

    C

--- src/api/apiurls.jsx
+++ src/api/apiurls.jsx
@@ -1,6 +1,6 @@
 // Server MQTT INSTANCE
 export const mqttURL = "ws://38.43.134.172:1884"
-export const mqttDominio = "ws://telemetriaperu.com:1883"
+export const mqttDominio = "ws://telemetriaperu.com:1884"
 
 // Para pruebas locales
 export const mqttLocalURL = "ws://localhost:1883"
